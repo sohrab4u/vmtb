67ad99af5fcc950521bd8554924fdfda20d2859a
Mohd Sohrab Alam <sohrabalam.it@gmail.com>
2025-11-04 14:57:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def to_pdf(summary, report_data, unknown_info):
     return pdf_bytes.read()
 
 def main():
-    st.title("VMTB Data Analysis Dashboard - Uttar Pradesh")
+    st.title("VMTB Data Analysis - Uttar Pradesh")
 
     st.markdown("Upload the two Excel files:")
     file1 = st.file_uploader("Upload VMTB Village List Excel", type=["xlsx"])
